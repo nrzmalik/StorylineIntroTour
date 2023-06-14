9a02a3e351c0decbf3560eb35f2521056f9553d2
nrzmalik <125977246+nrzmalik@users.noreply.github.com>
2023-06-14 20:59:29 +0500

    Update nrzmalikStorylineIntorTour.js

--- nrzmalikStorylineIntorTour.js
+++ nrzmalikStorylineIntorTour.js
@@ -1,75 +1,75 @@
 function IntroTourWithSeekbar()  
 {
 
-var intro = introJs();
+introJs().setOptions({
+  steps: [
+    {
+      element: '.acc-blocker',
+      intro: 'Welcome to the e-learning course!',
+      title: 'Slide',
+    },
+    {
+      element: '#links-right',
+      intro: 'Here you can find helpful resources related to the course.',
+      title: 'Helpful Resources',
+    },
+    {
+      element: '#outline-content',
+      intro: 'The course menu is located here. It provides an overview of the course structure.',
+      title: 'Course Menu',
+    },
+    {
+      element: '#next',
+      intro: 'To move to the next slide, click the "Next" button.',
+      title: 'Next Slide Button',
+    },
+    {
+      element: '#prev',
+      intro: 'If you want to go back to the previous slide, use the "Previous" button.',
+      title: 'Prev Slide Button',
+    },
+    {
+      element: '#playback-controls',
+      intro: 'These controls allow you to play or pause the slide playback.',
+      title: 'Slide Playback Control',
+    }
+  ],
+}).start();
 
-  intro.setOptions({
-    steps: [
-      {
-        element: '.acc-blocker',
-        intro: 'Welcome to the course! This is the slide area where the course content will be displayed.',
-      },
-      {
-        element: '#links-right',
-        intro: 'Here you can find helpful resources related to the course. Click on the links to access additional materials or references.',
-      },
-      {
-        element: '#sidebar-panels',
-        intro: 'The course menu is located here. It provides an overview of the course structure and allows you to jump to specific sections or modules.',
-      },
-	  {
-        element: '#playback-controls',
-        intro: 'These controls allow you to play or pause the slide playback. You can control the pace of the course content based on your preferences.',
-      },
-      {
-        element: '#next',
-        intro: 'To move to the next slide, click the "Next" button. It will take you to the next topic or module in the course.',
-      },
-
-      {
-        element: '#prev',
-        intro: 'If you want to go back to the previous slide, use the "Previous" button. It will navigate you to the previous topic or module.',
-      }
-      
-    ]
-  });
-
-  intro.start();
 
 }
 
 function IntroTourWithoutSeekbar()
 {
+introJs().setOptions({
+  steps: [
+    {
+      element: '.acc-blocker',
+      intro: 'Welcome to the e-learning course!',
+      title: 'Slide',
+    },
+    {
+      element: '#links-right',
+      intro: 'Here you can find helpful resources related to the course.',
+      title: 'Helpful Resources',
+    },
+    {
+      element: '#outline-content',
+      intro: 'The course menu is located here. It provides an overview of the course structure.',
+      title: 'Course Menu',
+    },
+    {
+      element: '#next',
+      intro: 'To move to the next slide, click the "Next" button.',
+      title: 'Next Slide Button',
+    },
+    {
+      element: '#prev',
+      intro: 'If you want to go back to the previous slide, use the "Previous" button.',
+      title: 'Prev Slide Button',
+    }
+  ],
+}).start();
 
-var intro = introJs();
-
-  intro.setOptions({
-    steps: [
-      {
-        element: '.acc-blocker',
-        intro: 'Welcome to the course! This is the slide area where the course content will be displayed.',
-      },
-      {
-        element: '#links-right',
-        intro: 'Here you can find helpful resources related to the course. Click on the links to access additional materials or references.',
-      },
-      {
-        element: '#sidebar-panels',
-        intro: 'The course menu is located here. It provides an overview of the course structure and allows you to jump to specific sections or modules.',
-      },
-      {
-        element: '#next',
-        intro: 'To move to the next slide, click the "Next" button. It will take you to the next topic or module in the course.',
-      },
-
-      {
-        element: '#prev',
-        intro: 'If you want to go back to the previous slide, use the "Previous" button. It will navigate you to the previous topic or module.',
-      }
-      
-    ]
-  });
-
-  intro.start();
 
 }
