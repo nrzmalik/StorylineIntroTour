040e30e74597a10a91e4e08cfa7c6595913bc447
nrzmalik <125977246+nrzmalik@users.noreply.github.com>
2023-06-14 21:51:53 +0500

    Update nrzmalikStorylineIntorTour.js

--- nrzmalikStorylineIntorTour.js
+++ nrzmalikStorylineIntorTour.js
@@ -5,32 +5,32 @@ introJs().setOptions({
   steps: [
     {
       element: '.acc-blocker',
-      intro: 'Welcome to the e-learning course!',
+      intro: 'Welcome to the course! This is the slide area where the course content will be displayed. Use the navigation buttons to move between slides.',
       title: 'Slide',
     },
     {
       element: '#links-right',
-      intro: 'Here you can find helpful resources related to the course.',
+      intro: 'Here you can find helpful resources related to the course. Click on the links to access additional materials or references.',
       title: 'Helpful Resources',
     },
     {
       element: '#outline-content',
-      intro: 'The course menu is located here. It provides an overview of the course structure.',
+      intro: 'The course menu is located here. It provides an overview of the course structure and allows you to jump to specific sections or modules.',
       title: 'Course Menu',
     },
     {
       element: '#next',
-      intro: 'To move to the next slide, click the "Next" button.',
+      intro: 'To move to the next slide, click the "Next" button. It will take you to the next topic or module in the course.',
       title: 'Next Slide Button',
     },
     {
       element: '#prev',
-      intro: 'If you want to go back to the previous slide, use the "Previous" button.',
+      intro: 'If you want to go back to the previous slide, use the "Previous" button. It will navigate you to the previous topic or module.',
       title: 'Prev Slide Button',
     },
     {
       element: '#playback-controls',
-      intro: 'These controls allow you to play or pause the slide playback.',
+      intro: 'These controls allow you to play or pause the slide playback. You can control the pace of the course content based on your preferences.',
       title: 'Slide Playback Control',
     }
   ],
@@ -41,31 +41,32 @@ introJs().setOptions({
 
 function IntroTourWithoutSeekbar()
 {
-introJs().setOptions({
+
+  introJs().setOptions({
   steps: [
     {
       element: '.acc-blocker',
-      intro: 'Welcome to the e-learning course!',
+      intro: 'Welcome to the course! This is the slide area where the course content will be displayed. Use the navigation buttons to move between slides.',
       title: 'Slide',
     },
     {
       element: '#links-right',
-      intro: 'Here you can find helpful resources related to the course.',
+      intro: 'Here you can find helpful resources related to the course. Click on the links to access additional materials or references.',
       title: 'Helpful Resources',
     },
     {
       element: '#outline-content',
-      intro: 'The course menu is located here. It provides an overview of the course structure.',
+      intro: 'The course menu is located here. It provides an overview of the course structure and allows you to jump to specific sections or modules.',
       title: 'Course Menu',
     },
     {
       element: '#next',
-      intro: 'To move to the next slide, click the "Next" button.',
+      intro: 'To move to the next slide, click the "Next" button. It will take you to the next topic or module in the course.',
       title: 'Next Slide Button',
     },
     {
       element: '#prev',
-      intro: 'If you want to go back to the previous slide, use the "Previous" button.',
+      intro: 'If you want to go back to the previous slide, use the "Previous" button. It will navigate you to the previous topic or module.',
       title: 'Prev Slide Button',
     }
   ],
